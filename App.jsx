df9b0c71e4a93546a9f057b828b7fed4109a120b
sanya143-bit <sg4451865@gmail.com>
2026-07-27 22:19:19 +0530

    Create initial App component with layout

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="landing-content">
+        <h1>Paradise Nursery</h1>
+        <p>Where Green Meets Serenity</p>
+        <button className="get-started-btn">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
